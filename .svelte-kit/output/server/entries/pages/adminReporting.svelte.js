de6cd95a1dce18a70f990ec67446112dccea4bdf
jumana123 <60611954+jumana123@users.noreply.github.com>
2022-04-27 17:13:56 -0400

    new file initial commit

---
+++ .svelte-kit/output/server/entries/pages/adminReporting.svelte.js
@@ -0,0 +1,17 @@
+import { c as create_ssr_component } from "../../chunks/index-1f8e348f.js";
+import "../../chunks/firebase-4ef98d22.js";
+import "firebase/firestore";
+/* empty css                                                         */import "firebase/app";
+import "firebase/auth";
+var adminReporting_svelte_svelte_type_style_lang = "";
+const css = {
+  code: "#title.svelte-pf67d9{text-align:center;font-weight:700}h1.svelte-pf67d9{@apply font-semibold text-xl;}body.svelte-pf67d9{@apply flex flex-col place-items-center place-content-center h-screen w-screen;}div.svelte-pf67d9{@apply w-full max-w-xs flex flex-col gap-2;}",
+  map: null
+};
+const AdminReporting = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  $$result.css.add(css);
+  return `<body class="${"svelte-pf67d9"}"><div class="${"svelte-pf67d9"}"><h1 id="${"title"}" class="${"svelte-pf67d9"}">Admin Reporting System</h1></div>
+    
+</body>`;
+});
+export { AdminReporting as default };
